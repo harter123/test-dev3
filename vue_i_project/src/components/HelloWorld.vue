0e90a3856432cfc88c69e5a07eed067ca0bafc59
harter123 <harter@126.com>
2020-02-23 12:15:30 +0800

    第二节课

--- vue_i_project/src/components/HelloWorld.vue
+++ vue_i_project/src/components/HelloWorld.vue
@@ -1,58 +1,96 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        {{message}}
+        {{myProps}}
+        <div v-html="htmlMessage"></div>
+
+        <div :id="idMessage" name="hello" :class="classMessage">
+qqqqqqq
+        </div>
+
+
+        <div v-if="boolMessage">
+            v-if {{boolMessage}}
+        </div>
+        <div v-else>
+            v-else {{boolMessage}}
+        </div>
+
+        <div v-show="showMessage">
+            v-show {{showMessage}}
+        </div>
+
+
+        <div v-for="item in listMessage" :key="item">
+            {{item}}
+        </div>
+
+        <el-button onclick="alert(1)">纯js button</el-button>
+        <el-button @click="buttonEvent">vue button</el-button>
+
+
+        <el-input v-model="inputMessage"/>
+        <div>
+            {{inputMessage}}
+        </div>
+        <el-button @click="inputEvent">input button</el-button>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String,
+            myProps: String,
+        },
+        data() {
+            return {
+                message: "hi 海贼",
+                htmlMessage: "<span style='color: crimson'> hi 海贼</span>",
+                idMessage: "hiHelloWorld",
+                classMessage: 'hiHelloWorld',
+
+                boolMessage: false,
+                showMessage: false,
+
+                listMessage: ["a", "b", "c"],
+
+                inputMessage: "input",
+            }
+        },
+        methods: {
+            buttonEvent(){
+              this.showMessage = !this.showMessage;
+            },
+            inputEvent(){
+                this.inputMessage = "input"
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+    .hiHelloWorld {
+        color: aqua;
+    }
 </style>
--- vue_i_project/src/views/Home.vue
+++ vue_i_project/src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App" myProps="hahahah"/>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import HelloWorld from '../components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        components: {
+            HelloWorld
+        },
+        props:{
+            arg: String,
+        },
+        date(){
+            return {
+                message: 'message'
+            }
+        },
+        methods: {
+
+        },
+        created(){
+
+        },
+
+    }
 </script>
+
+<style scoped>
+    .mycss {
+
+    }
+</style>
