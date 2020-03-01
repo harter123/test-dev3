aa088d1fb4c52f4c31672aa43f9130b45dce54e2
harter123 <harter@126.com>
2020-03-01 12:10:43 +0800

    第三课

--- vue_i_project/src/App.vue
+++ vue_i_project/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -28,5 +24,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+  body {
+    display: block;
+    margin: 0;
 }
 </style>
---
+++ vue_i_project/src/views/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login-container">
+aaaaa
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+    .login-container {
+        min-height: 100%;
+        width: 100%;
+        background-color: #2d3a4b;
+        overflow: hidden;
+    }
+</style>
